5a2dab4c914d365bfb24fc9bce1447296e7610d7
jamadrac <rabbikindalo001@gmail.com>
2023-12-13 17:52:27 +0200

    as

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rabbi\Desktop\focus\idoit\Startup-Fund
+set VIRTUAL_ENV=C:\Users\rabbi\Desktop\focus\sir\Startup-Fund
 
 if not defined PROMPT set PROMPT=$P$G
 
